9f5a00fee6bf8facdd065c3d686b39c131d1dfbf
RussuDev <45974008+RussuDev@users.noreply.github.com>
2018-12-24 18:13:20 +0300

    0.0.3

--- script/menu.js
+++ script/menu.js
@@ -1,5 +1,6 @@
 
 var canvas = document.getElementById('canvas')
 var cnvs = canvas.getContext('2d')
-cnvs.fillStyle='red'
-cnvs.fillRect(100,100,400,300)
+
+//загрузить фон
+var fon_menu = new Image()
