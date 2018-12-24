3e6e551a15b2fca438e67f745f3ef028c38eed87
RussuDev <45974008+RussuDev@users.noreply.github.com>
2018-12-24 21:28:05 +0300

    Update menu.js

--- script/menu.js
+++ script/menu.js
@@ -2,5 +2,14 @@
 var canvas = document.getElementById('canvas')
 var cnvs = canvas.getContext('2d')
 
-//загрузить фон
+//создать фон
 var fon_menu = new Image()
+//загрузить фон
+fon_menu.src = "img/menu/fon_menu.png"
+fon_menu.onload=function() {
+	//нарисовать фон
+	cnvs.drawImage(fon_menu,0,0)
+}
+
+
+
